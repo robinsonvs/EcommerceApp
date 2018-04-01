9c30da1b7b74e3e2e45a3266ef509704feb1aa86
Robinson Severo <robinsonvs@gmail.com>
2018-04-01 00:34:22 -0300

    upload project react 01012018 0034

--- src/HeaderHome.js
+++ src/HeaderHome.js
@@ -6,7 +6,7 @@ const HeaderHome = (props) => {
     return (
         <div className="jumbotron">
         <div className="container">
-          <h1 className="display-3 text-center"><img src={logo} className="App-logo" alt="logo" width="200" /></h1>
+          <h1 className="display-3 text-center"><img src={logo} className="App-logo" alt="logo" width="150" /></h1>
           <p className="text-center"><a className="btn btn-anuncie btn-lg" to="/novo-anuncio" role="button">Anuncie Grátis &raquo;</a></p>
         </div>
         </div>        
